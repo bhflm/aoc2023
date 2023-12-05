960218f52e478efe81f054df85b67367e574d5f1
germi <germanbonin@gmail.com>
2023-12-05 13:47:03 -0300

    switch to bun test runner, add cases for processLine

---
+++ src/day1/trebuchet.test.ts
@@ -0,0 +1,41 @@
+import {describe, expect, test} from 'bun:test';
+import { processLine } from './trebuchet';
+
+describe('Should retrieve correct number for processing lines', () => {
+  test('9sixsevenz3 => 93', () => {
+    const result = processLine("9sixsevenz3");
+    expect(result).toBe(93);
+  });
+  test('ndpdvz2sevenn => 22', () => {
+    const result = processLine("ndpdvz2sevenn");
+    expect(result).toBe(22);   
+  });
+  test('seven8zllmz => 88', () => {
+    const result = processLine("seven8zllmz");
+    expect(result).toBe(88);   
+  });
+  test('nineeight3vxrdvttwo => 33', () => {
+    const result = processLine("nineeight3vxrdvttwo");
+    expect(result).toBe(33);   
+  });
+  test('tntsjnine3jksrrvone => 33', () => {
+    const result = processLine("tntsjnine3jksrrvone");
+    expect(result).toBe(33);   
+  });
+
+  test('three8sdone => 88', () => {
+    const result = processLine("three8sdone");
+    expect(result).toBe(88);      
+  });
+
+  test('nine4nine => 44', () => {
+    const result = processLine("nine4nine");
+    expect(result).toBe(44);      
+  });
+
+  test('fourvone2vbpltlrj => 22', () => {
+    const result = processLine("fourvone2vbpltlrj");
+    expect(result).toBe(22);      
+  });
+
+})
---
+++ src/day1/trebuchet.ts
@@ -0,0 +1,82 @@
+import fs from 'fs';
+
+async function loadFile(filePath: string) {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
+  } catch(err) {
+    throw Error(JSON.stringify(err))
+  }
+};
+
+// checks if char is digit or not
+function isDigit(c: string): boolean {
+  if (!/\d/.test(c)) {
+    return false;
+  }
+
+  return true;
+}
+
+export function processLine(line: string): number {
+  let first: string | undefined;
+  let last: string | undefined;
+  let n: number = 0;
+  let prematureFound = false;
+  
+  const splitted: string[] = line.split('');
+
+  // TWO POINTER APPROACH; find first digit and last digit
+  let i = 0;
+  let j = splitted.length - 1;
+  
+  while (i < j && !prematureFound) {
+    if (first && last) {
+      prematureFound = true;
+    }
+
+    if (isDigit(splitted[i]) && !first){
+      first = splitted[i];
+    } 
+    
+    if (!first) {
+      i ++;
+    }
+
+
+    if (isDigit(splitted[j]) && !last){
+      last = splitted[j];
+    }
+
+    if (!last) {
+        j --;
+    }
+
+  };
+
+  if (first && !last) {
+    last = first;
+  }
+
+  if (last && !first) {
+    first = last;
+  }
+
+  n = parseInt(`${first}${last}`, 10);
+  return n;
+};
+
+async function main() {
+  let count = 0;
+  const filePath = 'input.txt';
+  const files = await loadFile(filePath);
+  const arr = files.split(/\r?\n/);
+  const err = [];
+  arr.forEach((line, i) => {
+    const res = processLine(line);
+    if (Number.isNaN(res)) {
+      err.push(line);
+    }
+    count += res;
+  });
+};
+
